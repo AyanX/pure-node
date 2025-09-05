1d34be463238077b4cba0a4c354098c414bf5441
Xhady Ayan <xhadyayan@gmail.com>
2025-09-05 23:00:17 +0300

    streams

---
+++ streams/write-to-file/main.js
@@ -0,0 +1,48 @@
+
+console.time("test")
+
+const fs = require("fs/promises")
+const Main = async()=>{
+
+    const file = await fs.open("test.txt","w")
+    const stream = file.createWriteStream()
+
+    let i =0
+
+    const write =async ()=>{
+        while(i<1000000){
+
+            const buff = Buffer.from(`${i} \n`)
+
+           if (!stream.writable){
+            break
+           }
+
+           
+
+           i++
+            stream.write(buff)
+            if (i == 1000000-1){
+                stream.end()
+           }
+        }
+    }
+    write()
+
+    stream.on("drain",()=>{
+        write()
+    })
+
+    stream.on("finish",()=>{
+        console.log("done")
+    })
+
+
+
+    await file.close()
+
+}
+Main()
+
+
+console.timeEnd("test")
